e645058aa7845767576b6986a459ed93c4270674
tuta0030 <jokerisfun@126.com>
2020-12-26 11:33:17 +0800

    update

--- web_src/js_files/test.js
+++ web_src/js_files/test.js
@@ -1,12 +1,22 @@
 // TODO 丰富自动生成的html内容
 // 导航栏 {主页， 作品展示， 联系}
+// 使用JS 生成传送门html元素
 
-console.log("Using js file...")
+console.log("Using js file")
 
 var top_stuff = document.getElementById("banner_img");
-var new_content_node = document.createElement("div");
-var new_content_text = document.createTextNode("HOME     GALLARY     CONTACT");
-new_content_node.setAttribute("class", "generated_text");
-new_content_node.appendChild(new_content_text);
+var footer = document.getElementById("footer");
+var navigating_bar = document.createElement("div");
 
-top_stuff.before(new_content_node);
+function setup_nav_bar () {
+    navigating_bar.innerHTML = "Banana";
+    navigating_bar.id = "generated_navigating_bar";
+};
+
+function setup_referral_links () {
+    document.getElementById("footer").innerHTML = " ";
+};
+
+setup_referral_links();
+setup_nav_bar();
+top_stuff.before(navigating_bar);
