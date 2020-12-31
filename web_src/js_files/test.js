c8156ed895f4c9c9d0a2d390b717bb93ec16720a
tuta0030 <jokerisfun@126.com>
2020-12-31 12:37:05 +0800

    update

--- web_src/js_files/test.js
+++ web_src/js_files/test.js
@@ -1,45 +1,19 @@
-function addFileDiv() {
-  let fileDiv = document.createElement("div");
-  let input = document.createElement("input");
-  let content = document.createElement("h3");
-  let pre = document.createElement("pre");
-  pre.id = "file-content";
-  content.innerHTML = "contents of the file: ";
-  input.type = "file";
-  input.id = "file-input";
-  content.appendChild(pre);
-  input.appendChild(content);
-  fileDiv.appendChild(input);
-  document.body.appendChild(fileDiv);
+function setupGallary () {
+    console.log("Setting up gallary");
+    let gallary = document.createElement("div");
+    gallary.id = "gallary";
+    getGallaryImages();
 }
 
-function readSingleFile(e) {
-  console.log("Using test.js");
-  addFileDiv();
-
-  let file = e.target.files[0];
-  if (!file) {
-    return;
-  }
-
-  let reader = new FileReader();
-  reader.onload = function (e) {
-    let content = e.target.result;
-    displayContent(content);
-  };
-  reader.readAsText(file);
+function getGallaryImages() {
+    let imgsDir = "web_src/images/gallary_images/";
+    for (i=1; i<11; i++) {
+        let image = document.createElement("img");
+        image.src = imgsDir.concat(i.toString()).concat(".jpg");
+        document.body.appendChild(image);
+    }
 }
-
-function displayContent(contents) {
-  let element = document.getElementById("file-content");
-  element.textContent = contents;
-  window.open("data:text/html," + encodeURIComponent(data), "_blank");
-}
-
-// document
-//   .getElementById("file-input")
-//   .addEventListener("change", readSingleFile, false);
 
 define(function () {
-  return { banana: readSingleFile };
+  return { banana: setupGallary };
 });
