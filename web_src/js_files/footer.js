7e410fc5bc204fa459a5f3acadd2d0530712ab49
tuta0030 <jokerisfun@126.com>
2021-01-03 12:16:32 +0800

    update

---
+++ web_src/js_files/footer.js
@@ -0,0 +1,14 @@
+function setup_footer () {
+    let footer = document.createElement("div");
+    let textDiv = document.createElement("div");
+    let hr = document.createElement("hr");
+    footer.id = "footer";
+    textDiv.className = "portal";
+    // textDiv.style = "text-align: center; color: rgb(136, 136, 136); padding-top: 5vh"
+    textDiv.innerHTML = "传送门"
+    footer.appendChild(textDiv);
+    footer.appendChild(hr);
+    document.body.appendChild(footer);
+  }
+
+define (function () { return {setup_footer: setup_footer}});
